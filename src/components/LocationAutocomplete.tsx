58d9094457046a6a39a51c68c6f2fcda49973d5f
Max Ward <max.jp.ward@gmail.com>
2025-07-19 22:04:48 -0700

    add loading indicator to search bar while API results are fetched

--- src/components/LocationAutocomplete.tsx
+++ src/components/LocationAutocomplete.tsx
@@ -33,6 +33,7 @@ export const LocationAutocomplete: React.FC<LocationAutocompleteProps> = ({
     useState<LocationFeature | null>(null);
   const [suggestions, setSuggestions] = useState<LocationFeature[]>([]);
   const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     // Only fetch if input is long enough
@@ -44,6 +45,7 @@ export const LocationAutocomplete: React.FC<LocationAutocompleteProps> = ({
     // Create a function to fetch suggestions
     const fetchSuggestions = async () => {
       try {
+        setIsLoading(true);
         const response = await fetch(
           `https://photon.komoot.io/api/?q=${encodeURIComponent(inputValue)}`,
         );
@@ -56,9 +58,11 @@ export const LocationAutocomplete: React.FC<LocationAutocompleteProps> = ({
         );
 
         setSuggestions(filteredSuggestions);
+        setIsLoading(false);
       } catch (error) {
         console.error("Error fetching suggestions:", error);
         setSuggestions([]);
+        setIsLoading(false);
       }
     };
 
@@ -120,10 +124,11 @@ export const LocationAutocomplete: React.FC<LocationAutocompleteProps> = ({
         listboxProps={{
           emptyContent: "No OSM relations found.",
         }}
+        isLoading={isLoading}
         value={inputValue}
         onInputChange={handleInputChange}
-        onSelectionChange={(key) => {
-          const index = Number(key);
+        onSelectionChange={(key: number) => {
+          const index = key;
           if (!isNaN(index) && index >= 0 && index < suggestions.length) {
             setSelectedLocation(suggestions[index]);
           } else {
