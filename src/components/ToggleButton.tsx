e42d6b5407e28c92ba5be72a9d6c6dbebe85f39d
Max Ward <max.jp.ward@gmail.com>
2025-07-30 20:36:23 -0700

    add lane count to other lane button

--- src/components/ToggleButton.tsx
+++ src/components/ToggleButton.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@heroui/button";
+import { ReactNode } from "react";
 import kebab from "../assets/kebab.svg";
 
 const toggleButton = (
@@ -6,15 +7,12 @@ const toggleButton = (
   isActive: boolean,
   onPress?: () => void,
   isIconOnly: boolean = false,
+  slot?: ReactNode,
 ) => (
   <Button
     key={label}
     variant="bordered"
-    className={`flex-1 border-1 transition-all duration-200 ${
-      isActive
-        ? "bg-primary-100 shadow-lg border-primary"
-        : "hover:bg-primary/10"
-    }`}
+    className={`flex-1 border-1 transition-all duration-200 ${isActive ? "bg-primary-100 shadow-lg border-primary" : "hover:bg-primary/10"}`}
     onPress={onPress}
     isIconOnly={isIconOnly}
   >
@@ -25,7 +23,10 @@ const toggleButton = (
         className="h-6 w-6 brightness-0 dark:brightness-100 dark:invert"
       />
     ) : (
-      label
+      <div className="flex items-center gap-2">
+        <span>{label}</span>
+        {slot}
+      </div>
     )}
   </Button>
 );
